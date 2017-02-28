c9d2ff58e5e685e6dffaaf796b983fae00dd1563
Josh Lee <joshswl@gmail.com>
2017-02-28 16:52:08 -0600

    progress...

---
+++ app/app.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="page">
+    <div class="panel">
+      <div class="panel-block"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SongItem from './song-item.vue';
+export default {
+  components: {
+    SongItem,
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ app/song-item.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="results">
+    <div class="track">
+      <div class="track__media">{{track.media}}</div>
+      <div class="track__frame">{{track.frame}}</div>
+      <div class="track__artist">{{track.artist}}</div>
+      <div class="track__name">{{track.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['track'],
+  created() {
+    fetch('https://api.spotify.com/v1/search?query=katy&type=track&offset=0&limit=20')
+      .then(response => response.json())
+      .then((tracks) => {
+        this.tracks = tracks;
+      });
+  },
+  data() {
+    return {
+      tracks: {},
+
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
