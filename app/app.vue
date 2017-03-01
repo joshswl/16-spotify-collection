881505c4816c0c68ef7c2d20839198019908ad86
Josh Lee <joshswl@gmail.com>
2017-02-28 18:26:24 -0600

    before fetching

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,24 @@
 <template lang="html">
-  <div class="page">
-    <div class="panel">
-      <div class="panel-block"></div>
+  <div class="app">
+    <div class="page">
+      <div class="panel">
+        <div class="results">
+          <div class="track">
+            <div class="panel-block media">
+              <div class="media-left">
+                <img src="http://placecage.com/400/400" alt="" class="track__media track__frame">
+              </div>
+              <div class="media-content">
+                <div class="track__artist">asdf artist</div>
+                <div class="track__name">track name name name</div>
+              </div>
+              <div class="media-right">
+                <span class="fa fa-spotify spotify-icon"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- app/song-item.vue
+++ app/song-item.vue
@@ -1,12 +1,5 @@
 <template lang="html">
-  <div class="results">
-    <div class="track">
-      <div class="track__media">{{track.media}}</div>
-      <div class="track__frame">{{track.frame}}</div>
-      <div class="track__artist">{{track.artist}}</div>
-      <div class="track__name">{{track.name}}</div>
-    </div>
-  </div>
+
 </template>
 
 <script>
@@ -16,13 +9,14 @@ export default {
     fetch('https://api.spotify.com/v1/search?query=katy&type=track&offset=0&limit=20')
       .then(response => response.json())
       .then((tracks) => {
-        this.tracks = tracks;
+        this.data = tracks;
       });
   },
   data() {
     return {
-      tracks: {},
-
+      tracks: [
+        {}
+      ],
     };
   },
 
